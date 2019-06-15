394b86dde3d04d1494d7c491dd3970704990f735
Gery Wahyu <gerywahyunugraha@gmail.com>
2019-06-15 16:18:00 +0700

    Add profile page scaffold

---
+++ src/views/ProfilePage/ProfilePage.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: 'profile',
+  computed: {
+    ...mapState("session", ["user"])
+  }
+}
+</script>
+
+<style>
+
+</style>
