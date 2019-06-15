b63a3718a26d7e93aa7113a0cf4476c391783289
Ahmad Fahmi <afahmip@Ahmads-MacBook-Pro.local>
2019-06-15 14:09:25 +0700

    Add base navbare

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="navbar">
+    <div class="navbar__item" v-for="item in items" :key="item.name">
+      <i :class="item.icon"/>
+      <p>{{item.name}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      items: [
+        {
+          name: "Home",
+          icon: "fas fa-home"
+        },
+        {
+          name: "Profile",
+          icon: "fas fa-user"
+        },
+        {
+          name: "Profile",
+          icon: "fas fa-user"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  display: flex;
+  justify-content: space-evenly;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+
+  &__item {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 1rem 0;
+
+    i {
+      margin-bottom: 0.25rem;
+      font-size: 1.25rem;
+    }
+    
+    p {
+      font-size: 0.85rem;
+    }
+  }
+}
+</style>
+
---
+++ src/views/HomePage/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <navbar />
+  </div>  
+</template>
+
+<script>
+import Navbar from '@/components/Navbar/Navbar.vue'
+
+export default {
+  name: 'HomePage',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
