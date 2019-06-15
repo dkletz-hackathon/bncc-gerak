42965004e273a8b28979f49788ce88f3f46450c6
Gery Wahyu <gerywahyunugraha@gmail.com>
2019-06-15 15:46:51 +0700

    Add login related stores

--- src/store.js
+++ src/store.js
@@ -1,16 +1,13 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import SessionStore from "./store/shared/session";
+import LoginStore from "./store/views/login";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    session: SessionStore,
+    login: LoginStore
   }
 })
